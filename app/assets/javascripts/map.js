297ab3c75da6b9c84d8132fa5de27fda452c1afb
James Christie <jwchristie.jc@gmail.com>
2014-09-15 10:18:29 +0100

    changing rounding of distance

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -27,7 +27,7 @@
           var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos((lat1) * (Math.PI/180)) * Math.cos((lat2) * (Math.PI/180)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
           var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
           var d = R * c;
-
+          d = (Math.round(d*100)/100).toFixed(2)
           data[i].distance = d;
           data[i].hide = true;
           data[i].hide_max = true;
