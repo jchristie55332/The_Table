ea4a4e8f70f7d73d6e371d8f6911d3ee004e874c
James Christie <jwchristie.jc@gmail.com>
2014-09-16 11:50:45 +0100

    improving the search with food styles

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,43 +1,41 @@
 
-
-  
   app.controller('mapController', ["$scope", "$http", function($scope, $http) {
+    $scope.food_styles = []
     $scope.map = {
       center: {latitude: 50.522420999999994, longitude: -0.109708 },
       zoom: 11
     };
-console.log("1")
     $http.get("/restaurants.json").success(function(data){
       navigator.geolocation.getCurrentPosition(function(position){
         $scope.$apply(function() {
           $scope.currentLat = position.coords.latitude
-        $scope.currentLng = position.coords.longitude
-        $scope.map.center = {latitude: $scope.currentLat, longitude: $scope.currentLng };
-        for (var i = 0; i < data.length; i++) {
+          $scope.currentLng = position.coords.longitude
+          $scope.map.center = {latitude: $scope.currentLat, longitude: $scope.currentLng };
+          for (var i = 0; i < data.length; i++) {
+            if ($scope.food_styles.indexOf(data[i].food_style) == -1) {
+              $scope.food_styles.push(data[i].food_style);
+            }
+            var lat1 = data[i].lat;
+            var lon1 = data[i].lng;
+            var lat2 = $scope.currentLat;
+            var lon2 = $scope.currentLng;  
+            var R = 6371;
 
-          var lat1 = data[i].lat;
-          var lon1 = data[i].lng;
-          var lat2 = $scope.currentLat;
-          var lon2 = $scope.currentLng;  
-          var R = 6371;
-
-          var dLat = (lat2-lat1) * (Math.PI/180);
-          var dLon = (lon2-lon1) * (Math.PI/180); 
-          var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos((lat1) * (Math.PI/180)) * Math.cos((lat2) * (Math.PI/180)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
-          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-          var d = R * c;
-          d = (Math.round(d*100)/100).toFixed(2)
-          data[i].distance = d;
-          data[i].hide = true;
-          data[i].hide_max = true;
-          $scope.hello = true;
+            var dLat = (lat2-lat1) * (Math.PI/180);
+            var dLon = (lon2-lon1) * (Math.PI/180); 
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos((lat1) * (Math.PI/180)) * Math.cos((lat2) * (Math.PI/180)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            var d = R * c;
+            d = (Math.round(d*100)/100).toFixed(2)
+            data[i].distance = d;
+            data[i].hide = true;
+            data[i].hide_max = true;
+            $scope.hello = true;
         }
       })
       });
-
       $scope.restaurants = data;
-      console.log("3")
-
+      $scope.food_styles = $scope.food_styles.filter(function(item, i, ar){ return ar.indexOf(item) === i; })
     });
 
     $scope.selectRestaurant = function(restaurant){
--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,5 +1,11 @@
 app.controller('reservationsController', ["$scope", "$http", function($scope, $http) {
+  $scope.dates = []
   $http.get("/restaurant_reservations.json").success(function(data){
     $scope.reservations = data;
+    for (var i = 0; i < data.length; i++) {
+      if ($scope.dates.indexOf(data[i].date) == -1) {
+        $scope.dates.push(data[i].date);
+      }
+    };
   });
 }]);
