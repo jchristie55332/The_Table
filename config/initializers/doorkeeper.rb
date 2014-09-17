ed9ecf9c093a2376b41ca961cb126b5df1763a91
James Christie <jwchristie.jc@gmail.com>
2014-09-17 17:03:35 +0100

    still playing with env variables

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,89 +1,38 @@
 Doorkeeper.configure do
-  # Change the ORM that doorkeeper will use.
-  # Currently supported options are :active_record, :mongoid2, :mongoid3, :mongo_mapper
-  orm :active_record
-
-  # This block will be called to check whether the resource owner is authenticated or not.
+  # This block will be called to check whether the
+  # resource owner is authenticated or not
   resource_owner_authenticator do |routes|
-      User.find(current_user.id) || redirect_to(user_session_path)
-    end
+    # Put your resource owner authentication logic here.
+    # If you want to use named routes from your app you need
+    # to call them on routes object eg.
+    # routes.new_user_session_path
+    current_user || warden.authenticate!(:scope => :user)
+  end
 
-  # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
+  # If you want to restrict the access to the web interface for
+  # adding oauth authorized applications you need to declare the
+  # block below
+  # admin_authenticator do |routes|
   #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
+  #   # If you want to use named routes from your app you need
+  #   # to call them on routes object eg.
+  #   # routes.new_admin_session_path
+  #   Admin.find_by_id(session[:admin_id]) || redirect_to routes.new_admin_session_path
   # end
 
-  # Authorization Code expiration time (default 10 minutes).
-  # authorization_code_expires_in 10.minutes
+  # resource_owner_from_credentials do
+  #   warden.authenticate!(:scope => :user)
+  # end
 
-  # Access token expiration time (default 2 hours).
-  # If you want to disable expiration, set this to nil.
+  # Access token expiration time (default 2 hours)
   # access_token_expires_in 2.hours
-
-  # Reuse access token for the same resource owner within an application (disabled by default)
-  # Rationale: https://github.com/doorkeeper-gem/doorkeeper/issues/383
-  # reuse_access_token
+  access_token_expires_in 5.minutes
 
   # Issue access tokens with refresh token (disabled by default)
-  # use_refresh_token
-
-  # Provide support for an owner to be assigned to each registered application (disabled by default)
-  # Optional parameter :confirmation => true (default false) if you want to enforce ownership of
-  # a registered application
-  # Note: you must also run the rails g doorkeeper:application_owner generator to provide the necessary support
-  # enable_application_owner :confirmation => false
+  use_refresh_token
 
   # Define access token scopes for your provider
-  # For more information go to
-  # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
-  # default_scopes  :public
-  # optional_scopes :write, :update
-
-  # Change the way client credentials are retrieved from the request object.
-  # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
-  # falls back to the `:client_id` and `:client_secret` params from the `params` object.
-  # Check out the wiki for more information on customization
-  # client_credentials :from_basic, :from_params
-
-  # Change the way access token is authenticated from the request object.
-  # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
-  # falls back to the `:access_token` or `:bearer_token` params from the `params` object.
-  # Check out the wiki for more information on customization
-  # access_token_methods :from_bearer_authorization, :from_access_token_param, :from_bearer_param
-
-  # Change the native redirect uri for client apps
-  # When clients register with the following redirect uri, they won't be redirected to any server and the authorization code will be displayed within the provider
-  # The value can be any string. Use nil to disable this feature. When disabled, clients must provide a valid URL
-  # (Similar behaviour: https://developers.google.com/accounts/docs/OAuth2InstalledApp#choosingredirecturi)
-  #
-  # native_redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
-
-  # Specify what grant flows are enabled in array of Strings. The valid
-  # strings and the flows they enable are:
-  #
-  # "authorization_code" => Authorization Code Grant Flow
-  # "implicit"           => Implicit Grant Flow
-  # "password"           => Resource Owner Password Credentials Grant Flow
-  # "client_credentials" => Client Credentials Grant Flow
-  #
-  # If not specified, Doorkeeper enables all the four grant flows.
-  #
-  # grant_flows %w(authorization_code implicit password client_credentials)
-
-  # Under some circumstances you might want to have applications auto-approved,
-  # so that the user skips the authorization step.
-  # For example if dealing with trusted a application.
-  # skip_authorization do |resource_owner, client|
-  #   client.superapp? or resource_owner.admin?
-  # end
-
-  # WWW-Authenticate Realm (default "Doorkeeper").
-  # realm "Doorkeeper"
-
-  # Allow dynamic query parameters (disabled by default)
-  # Some applications require dynamic query parameters on their request_uri
-  # set to true if you want this to be allowed
-  # wildcard_redirect_uri false
+  # For more information go to https://github.com/applicake/doorkeeper/wiki/Using-Scopes
+  default_scopes  :public
+  optional_scopes :write
 end
