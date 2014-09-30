667aa5466e03731a8d8181da06bd52c1b18ccafc
James Christie <jwchristie.jc@gmail.com>
2014-09-30 15:48:19 +0100

    api v1

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,12 @@
+module Api::V1
+  class ReservationsController < ApiController
+    doorkeeper_for :all
+    respond_to :json
+
+    def index
+      @restaurant = Restaurant.find(current_user.restaurant_id)
+      @reservations = Reservation.  where(restaurant_id: @restaurant.id)
+      respond_with @reservations
+    end
+  end
+end
